e6fdab41ae3422f6a05d72b2e00cb004c45c8be4
cristhianlopez14 <cristhianl0306@gmail.com>
2022-09-18 21:51:48 -0500

    ajustes en js

--- Assets/js/ejercicio6.js
+++ Assets/js/ejercicio6.js
@@ -1,3 +1,58 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+	password: /^.(([a-z]{1,})([A-Z]{1,})([0-9]{1,}){1,10})$/, 
+	correo: /^[a-zA-Z0-9_.+-]+@[m]+[i]+[s]+[e]+[n]+[a]+\.[e]+[d]+[u]+\.[c]+[o]+$/,
+	ndocumento: /^.{5,15}$/,
+   // fecha: /^[ccttpp]+$/,
+    //tdocumento: /^[ccttpp]+$/
+}
+
+const campos = {
+	nombre: false,
+    apellido: false,
+	password: false, 
+	correo: false,
+	ndocumento: false,
+    //fecha: false,
+    //tdocumento: false
+}
+
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+        case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "ndocumento":
+			validarCampo(expresiones.ndocumento, e.target, 'ndocumento');
+		break;
+        /*case "fecha":
+            validarCampo(expresiones.fecha, e.target, 'fecha');
+        break;
+        case "tdocumento":
+            validarCampo(expresiones.tdocumento, e.target, 'tdocumento');
+        break; */ 
+	}
+}
+
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
@@ -43,10 +98,10 @@ inputs.forEach((input) => {
 });
 
 formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
+	e.preventDefault();  
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+	if(campos.nombre && campos.apellido &&  campos.password && campos.correo && campos.ndocumento && terminos.checked  ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
@@ -59,5 +114,6 @@ formulario.addEventListener('submit', (e) => {
 		});
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        
 	}
 });
